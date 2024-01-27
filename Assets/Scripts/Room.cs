aa096d07292d03bbb8c764d1f252e9e17b617d33
jane901201 <75302011+jane901201@users.noreply.github.com>
2024-01-27 16:35:23 +0800

    新增 isSolvingPuzzle 跟 OnPuzzleGame

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Room : MonoBehaviour
 {
     public GameObject eventCreateCheckPoint;
     public GameObject createRoomCheckPoint;
-    public GameObject roomCreatePoint;
 }
--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -2,15 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class RoomManager : MonoBehaviour
 {
     [Tooltip("房間物件")]
     public GameObject room;
     public TriggerPoint triggerPoint;
-    public int moveRange;
+    public int createRoomNum;
+    public int createRoomRange;
+    public Vector3 initinalRoom;
+    public int currentIndex = 0;
+    public bool isSucceed = true;
 
-    private Queue<Room> rooms;
+    private LinkedList<Room> rooms;
+    public static bool isSolvingPuzzle = false;
+    public static Action OnPuzzleGame;
+    
+    private static RoomManager instance;
+    
+    public static RoomManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new RoomManager();
+            }
+            return instance;
+        }
+    }
 
     public void Awake()
     {
@@ -21,40 +42,79 @@ public class RoomManager : MonoBehaviour
     {
         InitinalRoom();
     }
-
-
+    
     public void TriggerPointEvent(string name)
     {
         if (name == "CreateRoomCheckPoint")
         {
             CreateRoom();
-            DestroyRoom();
         }
         else if (name == "EventCheckPoint")
         {
-            //TODO:回頭接 GameManager 的遊戲事件觸發
-            PuzzleGameEvent();
+            isSolvingPuzzle = true;
+            OnPuzzleGame.Invoke();
         }
     }
 
     public void InitinalRoom()
     {
-        
+        //TODO:設定 Puzzle 的位置
+        //SetPuzzleGameCheckPosition();
+        GameObject tempRoom = Instantiate(room);
+        tempRoom.transform.parent = transform;
+        tempRoom.transform.position = new Vector3(0, 0, 0);
+        rooms.AddFirst(tempRoom.GetComponent<Room>());
+        for (int i = 0; i < createRoomNum; i++)
+        {
+            tempRoom = Instantiate(room);
+            tempRoom.transform.parent = transform;
+            tempRoom.transform.position = new Vector3(0, 0, (i + 1)*createRoomRange);
+            rooms.AddFirst(tempRoom.GetComponent<Room>());
+        }
+        for (int i = 0; i > -createRoomNum; i--)
+        {
+            tempRoom = Instantiate(room);
+            tempRoom.transform.parent = transform;
+            tempRoom.transform.position = new Vector3(0, 0, (i - 1)*createRoomRange);
+            rooms.AddLast(tempRoom.GetComponent<Room>());
+        }
     }
     
     //生成房間，更改事件觸發的距離
+    public void CreateForwordRoom()
+    {
+        GameObject tempRoom = Instantiate(room);
+        tempRoom.transform.parent = transform;
+        tempRoom.transform.position = new Vector3(0, 0, (currentIndex + 2)*createRoomRange);
+        Room destoryRoom = rooms.Last.Value;
+        rooms.AddFirst(tempRoom.GetComponent<Room>());
+        rooms.RemoveLast();
+        Destroy(destoryRoom);
+        currentIndex++;
+    }
+    
+    public void CreateBackRoom()
+    {
+        GameObject tempRoom = Instantiate(room);
+        tempRoom.transform.parent = transform;
+        tempRoom.transform.position = new Vector3(0, 0, (currentIndex + 2)*createRoomRange);
+        Room destoryRoom = rooms.First.Value;
+        rooms.AddLast(tempRoom.GetComponent<Room>());
+        rooms.RemoveFirst();
+        Destroy(destoryRoom);
+        currentIndex--;
+    }
+
     public void CreateRoom()
     {
-        //
+        if(isSucceed)
+            CreateForwordRoom();
+        else
+            CreateBackRoom();
     }
-
-    public void DestroyRoom()
+    
+    public void SetPuzzleGameCheckPosition()
     {
-        
-    }
-
-    public void PuzzleGameEvent()
-    {
-        
+        //設定 Puzzle 的隨機位置
     }
 }
