7ed28f518eeaedeb20ad75421ad2afef16fa639a
jane901201 <75302011+jane901201@users.noreply.github.com>
2024-01-27 17:41:13 +0800

    新增移動程式

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -17,7 +18,8 @@ public class GameManager : MonoBehaviour
     public List<PuzzleData> puzzleDatas;
     public int currentCorrectNum = 0;
     public MouseColor color;
-
+    public float playerSpeed = 5f;
+    public GameObject playerCamera;
 
     bool isPuzzing;
 
@@ -27,5 +29,27 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
         }
+    }
+    
+    private void Start()
+    {
+        PlayerMove();
+    }
+
+    public void PlayerMove()
+    {
+        playerCamera.transform.DOMove(playerCamera.transform.position + Vector3.forward * playerSpeed, 100.0f)
+            .SetEase(Ease.Linear)
+            .OnUpdate(() =>
+            {
+                // 在 Tween 更新時檢查條件
+                if (RoomManager.isSolvingPuzzle)
+                {
+                    // 停止 Tween
+                    playerCamera.transform.DOKill();
+                    Debug.Log("Tween 已停止");
+                }
+            })
+            .OnComplete(() => Debug.Log("移動完成"));
     }
 }
