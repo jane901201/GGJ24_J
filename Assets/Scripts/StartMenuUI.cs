42c24b235eeeb89f4ccfccf2681fcd935315c623
jane901201 <75302011+jane901201@users.noreply.github.com>
2024-01-28 12:16:10 +0800

    處理好開始場景的程式

---
+++ Assets/Scripts/StartMenuUI.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuUI : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    
+    
+    public void ExitGame()
+    {
+        // 在遊戲中呼叫 Application.Quit() 退出遊戲
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
